56c92ff53fe9042b1f1c91613ddd1afcd4c9ebba
willymaxwell <ezzellmax@gmail.com>
2024-06-13 20:29:24 -0400

    feat: Implement make_sandwich.

--- main.py
+++ main.py
@@ -76,5 +76,8 @@ class SandwichMachine:
     def make_sandwich(self, sandwich_size, order_ingredients):
         """Deduct the required ingredients from the resources.
            Hint: no output"""
+        for item in order_ingredients:
+            self.machine_resources[item] -= order_ingredients[item]
+        print(f"{sandwich_size} sandwich is ready. Bon appetit!")
 
 ### Make an instance of SandwichMachine class and write the rest of the codes ###
